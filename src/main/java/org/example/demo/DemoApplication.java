467f313dd9b2ebb312a9d8e7fe65a80c204cc9cb
piaorongzhen <prz_2022@qq.com>
2024-10-06 16:25:58 +0800

    480p->720p

--- src/main/java/org/example/demo/DemoApplication.java
+++ src/main/java/org/example/demo/DemoApplication.java
@@ -26,7 +26,7 @@ public class DemoApplication extends Application {
         // 启动窗口
         final ViewTuple<WindowView, WindowViewModel> viewTuple = FluentViewLoader.fxmlView(WindowView.class).load();
         final Parent root = viewTuple.getView();
-        Scene scene = new Scene(root, 640, 480);
+        Scene scene = new Scene(root, 1280, 720);
         stage.setTitle("Demo Application");
         stage.setScene(scene);
         stage.show();
