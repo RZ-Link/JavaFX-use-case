802067756d9a4b1f8740a8598a1747c7680c53da
piao <prz_2022@qq.com>
2025-11-20 19:47:55 +0800

    升级jdk21，javafx22，新增调试工具

--- src/main/java/org/example/demo/DemoApplication.java
+++ src/main/java/org/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package org.example.demo;
 import atlantafx.base.theme.PrimerLight;
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewTuple;
+import devtoolsfx.gui.GUI;
+import devtoolsfx.gui.Preferences;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -30,6 +32,10 @@ public class DemoApplication extends Application {
         stage.setTitle("Demo Application");
         stage.setScene(scene);
         stage.show();
+
+        // 启动调试工具
+        var preferences = new Preferences(getHostServices());
+        GUI.openToolStage(stage, preferences, "AtlantaFX");
     }
 
     public static void main(String[] args) {
